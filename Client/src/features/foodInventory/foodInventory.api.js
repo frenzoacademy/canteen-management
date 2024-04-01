95e7cb116b4cf8ab86d95bbf88a6a7e39802a894
boobalan-fz <boobalan.ramasamy@frenzo.io>
2024-04-01 10:08:56 +0530

    food inventory changes in api

--- Client/src/features/foodInventory/foodInventory.api.js
+++ Client/src/features/foodInventory/foodInventory.api.js
@@ -1,28 +1,28 @@
 import { axios } from "@/libs/axios";
 
 export const getFoodInventories = async () => {
-  const { data } = await axios.get(`/food-inventory`);
+  const { data } = await axios.get(`/foodInventory`);
   return data;
 };
 
 export const getFoodInventory = async (id) => {
-  const { data } = await axios.get(`/food-inventory/${id}`);
+  const { data } = await axios.get(`/foodInventory/${id}`);
   return data;
 };
 
 export const createFoodInventory = async (values) => {
-  const { data } = await axios.post(`/food-inventory`, values);
+  const { data } = await axios.post(`/foodInventory`, values);
   return data;
 };
 
 export const updateFoodInventory = async ({ id, values }) => {
-  const { data } = await axios.put(`/food-inventory/${id}`, values, {
+  const { data } = await axios.put(`/foodInventory/${id}`, values, {
     headers: { "Content-Type": "multipart/form-data" },
   });
   return data;
 };
 
 export const removeFoodInventory = async (id) => {
-  const { data } = await axios.delete(`/food-inventory/${id}`);
+  const { data } = await axios.delete(`/foodInventory/${id}`);
   return data;
 };
